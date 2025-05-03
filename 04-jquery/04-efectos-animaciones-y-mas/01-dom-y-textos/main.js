8a775f7bb99e55b1d3e73141ce92b0df21c44540
Antonio <antonicut@gmail.com>
2025-05-03 19:07:39 +0200

    Add - Efectos, Animaciones y más

---
+++ 04-jquery/04-efectos-animaciones-y-mas/01-dom-y-textos/main.js
@@ -0,0 +1,67 @@
+/*
+    -----------------------------------------------------------------------------
+    ----------  /04-master-javascript-clasico-jquery-typescript-mean/  ----------
+    ----------  /04-jquery/  ----------------------------------------------------
+    ----------  /04-efectos-animaciones-y-mas/  ---------------------------------
+    ----------  /01-dom-y-textos/  ----------------------------------------------
+    ----------  /index.html  ----------------------------------------------------
+    -----------------------------------------------------------------------------
+*/
+
+
+import { cdnJQuery_3_3_1 } from "/04-master-javascript-clasico-jquery-typescript-mean/04-jquery/src/libs/jquery/cdn/cdn-jquery-3.3.1.js";
+import { loadJQueryByCdnOLocal } from "/04-master-javascript-clasico-jquery-typescript-mean/04-jquery/src/libs/jquery/load/load-jquery-by-cdn-local.js";
+
+
+const cdnJQuery = cdnJQuery_3_3_1;
+const localJQuery = "/04-master-javascript-clasico-jquery-typescript-mean/04-jquery/src/libs/jquery/local/jquery-3.3.1.min.js";
+
+const scriptUrl = "/04-master-javascript-clasico-jquery-typescript-mean/04-jquery/src/scripts/04-efectos/01-dom-y-textos.js";
+
+
+//  ------------------------------------------------------------------------------------
+//  -----  Ejecutamos la Promesa de carga de jQuery y el script del proyecto  ----------
+//  ------------------------------------------------------------------------------------
+console.warn("Iniciando carga de jQuery...");
+console.log('\n')
+
+loadJQueryByCdnOLocal(cdnJQuery, localJQuery)
+
+    .then($ => {
+
+        console.log('\n');
+        console.warn("jQuery cargado correctamente - Version:", $.fn.jquery);
+
+        //  -----  cargamos el script principal del proyecto  -----
+        loadScript(scriptUrl);
+
+    })
+
+    .catch(err => console.error("Error al cargar jQuery:", err));
+
+
+
+//  ----------------------------------------------------------------------------------------
+//  ----------  Función que carga el script del proyecto de la lógica con jQuery  ----------
+//  ---------------------------------------------------------------------------------------- 
+function loadScript(scriptUrl) {
+
+    $.ajax({
+
+        url: scriptUrl,
+        type: 'HEAD',
+
+        success: function () {
+
+            $.getScript(scriptUrl)
+                .done(() => console.log(`Cargado: ${scriptUrl}`))
+                .fail((jqxhr, settings, exception) => console.error(`Error en ${scriptUrl}:`, exception));
+        },
+
+        error: function () {
+            console.warn(`No existe el script: ${scriptUrl}`);
+        }
+
+    });
+
+}
