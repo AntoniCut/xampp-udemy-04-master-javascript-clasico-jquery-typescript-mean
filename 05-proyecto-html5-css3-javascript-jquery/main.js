1272e6a8bdabb9e6e0a8111607ea970c009c739b
Antonio <antonicut@gmail.com>
2025-05-15 21:45:33 +0200

    Add - Plugin Slider y Posts Página Inicio del Proyecto

--- 05-proyecto-html5-css3-javascript-jquery/main.js
+++ 05-proyecto-html5-css3-javascript-jquery/main.js
@@ -7,53 +7,39 @@
 */
 
 
-import { cdnJQuery_3_3_1 } from "/04-master-javascript-clasico-jquery-typescript-mean/04-jquery/src/libs/jquery/cdn/cdn-jquery-3.3.1.js";
-import { loadJQueryByCdnOLocal } from "/04-master-javascript-clasico-jquery-typescript-mean/04-jquery/src/libs/jquery/load/load-jquery-by-cdn-local.js";
-
-import { cdnJQueryUI_1_12_1 } from "/04-master-javascript-clasico-jquery-typescript-mean/04-jquery/src/libs/jquery-ui/cdn/cdn-jquery-ui-1.12.1.js";
-import { loadJQueryUIByCdnOLocal } from "/04-master-javascript-clasico-jquery-typescript-mean/04-jquery/src/libs/jquery-ui/load/load-jquery-ui-by-cdn-local.js";
+import { cdnJQuery_3_1_1 } from "/04-master-javascript-clasico-jquery-typescript-mean/05-proyecto-html5-css3-javascript-jquery/src/libs/jquery/cdn/cdn-jquery-3.1.1.js";
+import { loadJQueryByCdnOLocal } from "/04-master-javascript-clasico-jquery-typescript-mean/05-proyecto-html5-css3-javascript-jquery/src/libs/jquery/load/load-jquery-by-cdn-local.js";
 
 
-const cdnJQuery = cdnJQuery_3_3_1;
-const localJQuery = "/04-master-javascript-clasico-jquery-typescript-mean/04-jquery/src/libs/jquery/local/jquery-3.3.1.min.js";
+const cdnJQuery = cdnJQuery_3_1_1;
+const localJQuery = "/04-master-javascript-clasico-jquery-typescript-mean/04-jquery/src/libs/jquery/local/jquery-3.1.1.min.js";
 
-const cdnJQueryUI = cdnJQueryUI_1_12_1;
-const localJQueryUI = "/04-master-javascript-clasico-jquery-typescript-mean/04-jquery/src/libs/jquery-ui/local/jquery-ui-1.12.1.min.js";
-
-const scriptUrl = "/04-master-javascript-clasico-jquery-typescript-mean/04-jquery/src/scripts/02-jquery-ui/01-jquery-ui-primeros-pasos.js";
+const bxSlider = "/04-master-javascript-clasico-jquery-typescript-mean/05-proyecto-html5-css3-javascript-jquery/src/plugins/bxslider-4-4.2.17/dist/jquery.bxslider.min.js";
+const scriptUrl = "/04-master-javascript-clasico-jquery-typescript-mean/05-proyecto-html5-css3-javascript-jquery/src/scripts/01-inicio.js";
 
 
 //  ------------------------------------------------------------------------------------
 //  -----  Ejecutamos la Promesa de carga de jQuery y el script del proyecto  ----------
 //  ------------------------------------------------------------------------------------
-console.warn("Iniciando carga de jQuery y jQueryUI...");
+console.warn("Iniciando carga de jQuery...");
 console.log('\n')
 
-loadJQueryByCdnOLocal( cdnJQuery, localJQuery )
+loadJQueryByCdnOLocal(cdnJQuery, localJQuery)
 
-    .then( $ => {
-        
+    .then($ => {
+
+        console.log('\n');
         console.warn("jQuery cargado correctamente - Version:", $.fn.jquery);
-        
-        //  -----  cargamos jQueryUI  -----
-        loadJQueryUIByCdnOLocal( cdnJQueryUI, localJQueryUI )
-            
-            .then( $ => {
-            
-                if (!$.ui) { 
-                    console.log('\n');
-                    throw new Error("jQuery UI no se cargó correctamente.");
-                }
-                
-                console.warn("jQuery UI cargado correctamente - Version:", $.ui.version);
-                    
-                //  -----  cargamos el script principal del proyecto  -----
-                loadScript(scriptUrl)
-            })
+
+        //  -----  cargamos el plugins para el slider  -----
+        loadScript(bxSlider);
+
+        //  -----  cargamos el script principal del proyecto  -----
+        loadScript(scriptUrl);
 
     })
 
-    .catch(err => console.error("Error al cargar jQuery o jQuery UI:", err));
+    .catch(err => console.error("Error al cargar jQuery:", err));
 
 
 
---
+++ 05-proyecto-html5-css3-javascript-jquery/src/scripts/01-inicio.js
@@ -0,0 +1,108 @@
+/*
+    -----------------------------------------------------------------------------
+    ----------  /04-master-javascript-clasico-jquery-typescript-mean/  ----------
+    ----------  /05-proyecto-html5-css3-javascript-jquery/  ---------------------
+    ----------  /src/scripts/  --------------------------------------------------
+    ----------  /01-inicio.js  --------------------------------------------------
+    -----------------------------------------------------------------------------
+*/
+
+
+(function ($) {
+
+    console.log('\n');
+    console.warn('-----  01-inicio.js  -----');
+    console.log('\n');
+
+
+    /*  
+        ----------------------------------------
+        ----------  plugins bxSlider  ----------
+        ----------------------------------------
+    */
+
+    const $galeria = $('.galeria');
+
+    $galeria.bxSlider({
+        //mode: 'horizontal',
+        //mode: 'vertical',
+        mode: 'fade',
+        caption: true,
+        slideWidth: 1230,
+        responsice: true,
+        pager: true
+    });
+
+
+    /*
+        -----------------------------
+        ----------  posts  ----------
+        -----------------------------
+    */
+
+    const $posts = $('#posts');
+    const arrPosts = [];
+    const day = moment().date();
+    const month = moment().format("MMM");
+    const year = moment().format("YYYY");
+
+
+    const posts = [
+
+        {
+            title: 'Prueba de titulo 1',
+            date: 'Publicado el dia ' + day + ' de ' + month + ' del ' + year,
+            content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit Harum ad et eveniet explicabo dignissimos fugit laudantium iure eos ullam ex aliquam ipsa tempore sapiente, rerum magnam officia totam, autem illo!. Lorem ipsum dolor sit amet consectetur adipisicing elit.Harum ad et eveniet explicabo dignissimos fugit laudantium iure eos.'
+        },
+
+        {
+            title: 'Prueba de titulo 2',
+            date: 'Publicado el dia ' + day + ' de ' + month + ' del ' + year,
+            content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit Harum ad et eveniet explicabo dignissimos fugit laudantium iure eos ullam ex aliquam ipsa tempore sapiente, rerum magnam officia totam, autem illo!. Lorem ipsum dolor sit amet consectetur adipisicing elit.Harum ad et eveniet explicabo dignissimos fugit laudantium iure eos.'
+        },
+
+        {
+            title: 'Prueba de titulo 3',
+            date: 'Publicado el dia ' + day + ' de ' + month + ' del ' + year,
+            content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit Harum ad et eveniet explicabo dignissimos fugit laudantium iure eos ullam ex aliquam ipsa tempore sapiente, rerum magnam officia totam, autem illo!. Lorem ipsum dolor sit amet consectetur adipisicing elit.Harum ad et eveniet explicabo dignissimos fugit laudantium iure eos.'
+        },
+
+        {
+            title: 'Prueba de titulo 4',
+            date: 'Publicado el dia ' + day + ' de ' + month + ' del ' + year,
+            content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit Harum ad et eveniet explicabo dignissimos fugit laudantium iure eos ullam ex aliquam ipsa tempore sapiente, rerum magnam officia totam, autem illo!. Lorem ipsum dolor sit amet consectetur adipisicing elit.Harum ad et eveniet explicabo dignissimos fugit laudantium iure eos.'
+        },
+
+        {
+            title: 'Prueba de titulo 5',
+            date: 'Publicado el dia ' + day + ' de ' + month + ' del ' + year,
+            content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit Harum ad et eveniet explicabo dignissimos fugit laudantium iure eos ullam ex aliquam ipsa tempore sapiente, rerum magnam officia totam, autem illo!. Lorem ipsum dolor sit amet consectetur adipisicing elit.Harum ad et eveniet explicabo dignissimos fugit laudantium iure eos.'
+        }
+    ]
+
+    console.log('posts =>', posts);
+    
+    posts.forEach((item, index) => {
+
+        const post = `
+                <article class="content__post">
+                    <h2 class='post__title'> ${item.title} </h2>
+                    <span class="post__date"> ${item.date} </span>
+                    <p class='post__description'> ${item.content} </p>
+                    <a class="post__more' href="#"> Leer más > </a>
+                </article>
+        `;
+
+        arrPosts.push(post);
+
+        console.log('post =>', post);
+
+    });
+
+    $posts.append(arrPosts);
+
+    
+})(jQuery);
+
+
+
