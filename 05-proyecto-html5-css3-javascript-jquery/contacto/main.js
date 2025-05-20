32dbca2650bfa9e500a59ad74a9a52545916cd97
Antonio <antonicut@gmail.com>
2025-05-20 22:32:50 +0200

    Add - Página de contacto - plugin jquery.form-validator - convertir a module y minificar el plugin

---
+++ 05-proyecto-html5-css3-javascript-jquery/contacto/main.js
@@ -0,0 +1,103 @@
+/*
+    -----------------------------------------------------------------------------
+    ----------  /04-master-javascript-clasico-jquery-typescript-mean/  ----------
+    ----------  /05-proyecto-html5-css3-javascript-jquery/  ---------------------
+    ----------  /contacto/  -----------------------------------------------------
+    ----------  /main.js  -------------------------------------------------------
+    -----------------------------------------------------------------------------
+*/
+
+
+//  -----  import libs jQuery  -----
+import { cdnJQuery_3_1_1 } from "/04-master-javascript-clasico-jquery-typescript-mean/05-proyecto-html5-css3-javascript-jquery/src/libs/jquery/cdn/cdn-jquery-3.1.1.js";
+import { loadJQueryByCdnOLocal } from "/04-master-javascript-clasico-jquery-typescript-mean/05-proyecto-html5-css3-javascript-jquery/src/libs/jquery/load/load-jquery-by-cdn-local.js";
+
+//  -----  import libs jQueryUI  -----
+import { cdnJQueryUI_1_14_1 } from "/04-master-javascript-clasico-jquery-typescript-mean/05-proyecto-html5-css3-javascript-jquery/src/libs/jquery-ui/cdn/cdn-jquery-ui-1.14.1.js";
+import { loadJQueryUIByCdnOLocal } from "/04-master-javascript-clasico-jquery-typescript-mean/05-proyecto-html5-css3-javascript-jquery/src/libs/jquery-ui/load/load-jquery-ui-by-cdn-local.js";
+
+//  -----  import plugins  -  bxSlider y jquery.form-validator  -----
+import { bxSlider } from "/04-master-javascript-clasico-jquery-typescript-mean/05-proyecto-html5-css3-javascript-jquery/src/plugins/bxslider-4-4.2.17/dist/jquery.bxslider-module.min.js";
+import { jQueryFormValidator } from "/04-master-javascript-clasico-jquery-typescript-mean/05-proyecto-html5-css3-javascript-jquery/src/plugins/jquery.form-validator-2.3.79/jquery.form-validator-module.min.js";
+
+
+const cdnJQuery = cdnJQuery_3_1_1;
+const localJQuery = "/04-master-javascript-clasico-jquery-typescript-mean/05-proyecto-html5-css3-javascript-jquery/src/libs/jquery/local/jquery-3.3.1.min.js";
+
+const cdnJQueryUI = cdnJQueryUI_1_14_1;
+const localJQueryUI = "/04-master-javascript-clasico-jquery-typescript-mean/05-proyecto-html5-css3-javascript-jquery/src/libs/jquery-ui/local/jquery-ui-1.14.1.min.js";
+
+
+//  -----  urls de los scripts a cargar  -----
+const scriptsUrls = [
+    '/04-master-javascript-clasico-jquery-typescript-mean/05-proyecto-html5-css3-javascript-jquery/src/scripts/slider.js',
+    '/04-master-javascript-clasico-jquery-typescript-mean/05-proyecto-html5-css3-javascript-jquery/src/plugins/jquery.form-validator-2.3.79/lang/es.js',
+    '/04-master-javascript-clasico-jquery-typescript-mean/05-proyecto-html5-css3-javascript-jquery/src/scripts/contacto.js',
+    '/04-master-javascript-clasico-jquery-typescript-mean/05-proyecto-html5-css3-javascript-jquery/src/scripts/selector-theme.js',
+    '/04-master-javascript-clasico-jquery-typescript-mean/05-proyecto-html5-css3-javascript-jquery/src/scripts/scroll.js',
+    '/04-master-javascript-clasico-jquery-typescript-mean/05-proyecto-html5-css3-javascript-jquery/src/scripts/login.js'
+];
+
+
+//  ------------------------------------------------------------------------------------
+//  -----  Ejecutamos la Promesa de carga de jQuery y el script del proyecto  ----------
+//  ------------------------------------------------------------------------------------
+console.warn("Iniciando carga de jQuery y jQueryUI...");
+console.log('\n')
+
+loadJQueryByCdnOLocal(cdnJQuery, localJQuery)
+
+    .then($ => {
+
+        console.warn("jQuery cargado correctamente - Version:", $.fn.jquery);
+
+        //  -----  cargamos jQueryUI  -----
+        loadJQueryUIByCdnOLocal(cdnJQueryUI, localJQueryUI)
+
+            .then($ => {
+
+                if (!$.ui) {
+                    console.log('\n');
+                    throw new Error("jQuery UI no se cargó correctamente.");
+                }
+
+                console.warn("jQuery UI cargado correctamente - Version:", $.ui.version);
+
+                //  -----  cargamos plugins bxSlider y jquery.form-validator convertido a module  -----
+                bxSlider($);
+                jQueryFormValidator($)
+
+                //  -----  cargamos el resto de scripts  -----
+                scriptsUrls.forEach(scriptUrl => loadScript(scriptUrl));
+            })
+
+    })
+
+    .catch(err => console.error("Error al cargar jQuery o jQuery UI:", err));
+
+
+
+//  ----------------------------------------------------------------------------------------
+//  ----------  Función que carga el script del proyecto de la lógica con jQuery  ----------
+//  ---------------------------------------------------------------------------------------- 
+function loadScript(scriptUrl) {
+
+    $.ajax({
+
+        url: scriptUrl,
+        type: 'HEAD',
+
+        success: function () {
+
+            $.getScript(scriptUrl)
+                .done(() => console.log(`Cargado: ${scriptUrl}`))
+                .fail((jqxhr, settings, exception) => console.error(`Error en ${scriptUrl}:`, exception));
+        },
+
+        error: function () {
+            console.warn(`No existe el script: ${scriptUrl}`);
+        }
+
+    });
+
+}
